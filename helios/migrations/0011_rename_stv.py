cf34a1f75ea378326655a14a697a88098f7015a8
Kostas Papadimitriou <kpap@grnet.gr>
2018-07-19 12:21:04 +0300

    migrations: Backward stv module migration

--- helios/migrations/0011_rename_stv.py
+++ helios/migrations/0011_rename_stv.py
@@ -3,14 +3,21 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
+
 def rename_stv(apps, schema_editor):
-    # We can't import the Person model directly as it may be a newer
-    # version than this migration expects. We use the historical version.
     Election = apps.get_model('helios', 'Election')
     Poll = apps.get_model('helios', 'Poll')
     Poll.objects.filter(election__election_module='stv').update(stv_droop=False)
     Election.objects.filter(election_module='stv').update(
         election_module='unicouncilsgr')
+
+
+def revert_stv(apps, schema_editor):
+    Election = apps.get_model('helios', 'Election')
+    Poll = apps.get_model('helios', 'Poll')
+    Election.objects.filter(election_module='unicouncilsgr').update(
+        election_module='stv')
+
 
 class Migration(migrations.Migration):
 
@@ -19,5 +26,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(rename_stv)
+        migrations.RunPython(rename_stv, revert_stv)
     ]
